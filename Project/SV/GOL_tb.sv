09218f9e9283dc991fb681f43c20609894df7ed8
VincErilla <143026095+VincErilla@users.noreply.github.com>
2023-12-07 21:18:22 -0600

    Wallahi

--- Project/SV/GOL_tb.sv
+++ Project/SV/GOL_tb.sv
@@ -24,25 +24,21 @@ module stimulus;
 
      initial
      begin
-          handle3 = $fopen("fsm.out");
-          #500 $finish;
+          handle3 = $fopen("tb.out");
+          #10000 $finish;
      end
 
-      always 
-     begin
-	desc3 = handle3;
-	#5 $fdisplay(desc3, "%b | L = %b R = %b H = %b || %b%b%b %b%b%b", 
-		     reset, q);
-     end   
+      always @(negedge clk)
+    begin
+    $fdisplay(desc3, "\n\n\n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b \n %b", q[255:240], q[239:224], q[223:208], q[207:192], q[191:176], 
+    q[175:160], q[159:144], q[143:128], q[127:112], q[111:96], q[95:80], q[79:64], q[63:48], q[47:32], q[31:16], q[15:0]);
+    end
+     
 
      initial 
      begin      
 	#0   reset = 1'b1;
-	#41  reset = 1'b0;	
-	#0   q = 1'b0;
-	#40  q = 1'b1;
-	#40  q = 1'b0;
-
+	#40  reset = 1'b0;	
      end
 
 
